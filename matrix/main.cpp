389c9a25292bfbdbe06712f69f0961e2fcf63764
BRAHMOUS <nitroschmitt@protonmail.com>
2024-12-08 17:30:42 +0100

    added construction from infinitely nested std::initializer_list

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -9,12 +9,21 @@
 int main(int argc, char* argv[])
 {
   //Matrix<int, 4> mtx(2, 2, 3, 4);
-  Matrix<int, 2> mtx(2, 2);
-  auto info = mtx.descriptor();
 
-  mtx.fill();
-  MatrixRef<int, 2> subMtx = mtx(0, 1);
+  //Matrix<int, 1> mtx(5);
+  //auto info = mtx.descriptor();
+
+  //mtx.fill();
+  //MatrixRef<int, 1> subMtx = mtx(2);
+
   //std::vector<size_t> vec { 0, 1, 0 };
 
   //std::cout << std::inner_product(vec.begin(), vec.end(), info.strides.begin(), 0) << std::endl;
+
+  Matrix<int, 2> mtx {
+    {54, 12, 33, 7},
+    {8, 56, 33, 14},
+    {8, 8, 7, 7},
+  };
+
 }
--- matrix/matrix.h
+++ matrix/matrix.h
@@ -5,13 +5,16 @@
 
 #define sztUNDEF (size_t) 0xFFFF
 
+template <bool B, typename T>
+using EnableIf = typename std::enable_if<B, T>::type;
+
 struct Slice {
 
   Slice() : start{ sztUNDEF }, size{ sztUNDEF }, stride{ 1 }
   {}
   explicit Slice(size_t s) : start{ s }, size{ sztUNDEF }, stride{ 1 }
   {}
-  Slice(size_t start, size_t size, size_t stride=1) : start{ start }, size{ size }, stride{ stride }
+  Slice(size_t start, size_t size, size_t stride = 1) : start{ start }, size{ size }, stride{ stride }
   {}
 
   size_t start;
@@ -26,7 +29,7 @@ struct MatrixSlice
 
   MatrixSlice(size_t s, std::initializer_list<size_t> extents);
   MatrixSlice(size_t s, std::initializer_list<size_t> extents, std::initializer_list<size_t> strides);
-  
+
   template <typename... Dims>
   MatrixSlice(Dims... dimensions);
 
@@ -95,7 +98,6 @@ size_t sliceDimension(const MatrixSlice<N>& oldslice, MatrixSlice<N>& newSlice, 
     newSlice.size = slice.size < newSlice.extents[current_dim] ? newSlice.size - (newSlice.extents[current_dim] - slice.size) * newSlice.strides[current_dim] : newSlice.size;
     newSlice.extents[current_dim] = std::ceil(static_cast<double>((slice.size - slice.start)) / static_cast<double>(slice.stride));
   }
-
   newSlice.strides[current_dim] = newSlice.strides[current_dim] * slice.stride;
 
   return 0;
@@ -115,6 +117,51 @@ size_t sliceMatrix(const MatrixSlice<N>& oldSlice, MatrixSlice<N>& newSlice, siz
   return 0;
 };
 
+template <typename T, size_t N>
+struct MatrixInitializerRec {
+  using type = std::initializer_list<typename MatrixInitializerRec<T, N - 1>::type>;
+};
+
+template <typename T>
+struct MatrixInitializerRec<T, 1> {
+  using type = std::initializer_list<T>;
+};
+
+template <typename T>
+struct MatrixInitializerRec <T, 0>;
+
+template <typename T, size_t N>
+using MatrixInitializer = typename MatrixInitializerRec<T, N>::type;
+
+template <size_t N, typename E, typename L>
+EnableIf< (N == 1), void> addExtents(E& first, L& list)
+{
+  *first++ = list.size();
+}
+
+template <size_t N, typename E, typename L>
+EnableIf< (N > 1), void> addExtents(E& first, L& list)
+{
+  auto start = list.begin();
+  for (auto i = start + 1; i != list.end(); ++i)
+    if (i->size() != start->size()) throw "error not all lists are the same size";
+
+  *first = list.size();
+  addExtents<N-1>(++first, *list.begin());
+}
+
+template <typename T>
+void copyFromInitList(const T* s_start, const T* s_end, std::vector<T>& destination)
+{
+  destination.insert(destination.end(), s_start, s_end);
+}
+
+template <typename L, typename T>
+void copyFromInitList(const std::initializer_list<L>* s_start, const std::initializer_list<L>* s_end, std::vector<T>& destination) {
+  for (; s_start != s_end; s_start++)
+    copyFromInitList(s_start->begin(), s_start->end(), destination);
+}
+
 
 template <typename T, size_t N>
 class Matrix
@@ -133,7 +180,10 @@ public:
   ~Matrix() = default;
 
   template <typename... Exts>
-  Matrix(Exts... extents);
+  explicit Matrix(Exts... extents);
+
+  Matrix(MatrixInitializer<T, N> list);
+  Matrix& operator=(MatrixInitializer<T, N>list);
 
   template<typename U>
   Matrix(std::initializer_list<U>) = delete;
@@ -155,12 +205,13 @@ public:
   }
 
   template <typename... Args >
-  MatrixRef<T, N> operator()(const Args&... args) 
+  MatrixRef<T, N> operator()(const Args&... args)
   {
-      constexpr size_t offset = N - (sizeof...(Args));
-      MatrixSlice<N> slice = _descriptor;
-      slice.start = sliceMatrix(_descriptor, slice, offset, args...);
-      return { slice, _elements.data()};
+    static_assert(sizeof...(Args) == N, "");
+    constexpr size_t offset = N - (sizeof...(Args));
+    MatrixSlice<N> slice = _descriptor;
+    slice.start = sliceMatrix(_descriptor, slice, offset, args...);
+    return { slice, _elements.data() };
   }
 
 private:
@@ -171,7 +222,7 @@ private:
 template<typename T, size_t N>
 template<typename ...Exts>
 inline Matrix<T, N>::Matrix(Exts ...extents)
-  : _descriptor{ extents... }, _elements( _descriptor.size )
+  : _descriptor{ extents... }, _elements(_descriptor.size)
 {}
 
 template<size_t N>
@@ -196,7 +247,7 @@ inline MatrixSlice<N>::MatrixSlice(Dims ...dimensions)
   static_assert(sizeof...(dimensions) == N, "ERROR");
   extents = { static_cast<size_t>(dimensions)... };
   strides[0] = 1;
-  
+
   for (long int i = 1; i < N; i++) {
     strides[i] = strides[i - 1] * *(extents.begin() + i - 1);
   }
@@ -209,3 +260,27 @@ inline MatrixSlice<N>::MatrixSlice(Dims ...dimensions)
 template<typename T, size_t N>
 inline MatrixRef<T, N>::MatrixRef(const MatrixSlice<N>& descriptor, T* pointer) : _descriptor{ descriptor }, _pointer{ pointer }
 {}
+
+template<typename T, size_t N>
+inline Matrix<T, N>::Matrix(MatrixInitializer<T, N> list)
+{
+  auto extents_begin = _descriptor.extents.begin();
+  addExtents<N>(extents_begin, list);
+  _descriptor.size = 1;
+  for (size_t e : _descriptor.extents)
+    _descriptor.size *= e;
+  _elements.reserve(_descriptor.size);
+  copyFromInitList(list.begin(), list.end(), _elements);
+}
+
+template<typename T, size_t N>
+inline Matrix<T, N>& Matrix<T, N>::operator=(MatrixInitializer<T, N> list)
+{
+  auto extents_begin = _descriptor.extents.begin();
+  addExtents<N>(extents_begin, list);
+  _descriptor.size = 1;
+  for (size_t e : _descriptor.extents)
+    _descriptor.size *= e;
+  _elements.reserve(_descriptor.size);
+  copyFromInitList(list.begin(), list.end(), _elements);
+}
